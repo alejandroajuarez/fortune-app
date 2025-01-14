e97580a2c60abf002db67bab7fdfdd401b940418
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-13 20:00:02 -0700

    add lotto winner route

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,18 @@
 class MyExamplesController < ApplicationController
+  def random
+    fortunes = [
+      "Please try again",
+      "You will find great fortune",
+      "Bad fortune for you and the next 10 generations of your family"
+    ]
+    render json: fortunes.sample
+  end
+  def lotto_numbers
+    winners = []
+    6.times do
+      winners << rand(0..61)
+    end
+
+    render json: winners
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/random_fortune", controller: "my_examples", action: "random"
+  get "/lotto_winners", controller: "my_examples", action: "lotto_numbers"
 end
